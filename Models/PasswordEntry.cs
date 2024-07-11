d3091dcf33ab934a9249c080b6079ab96e4a7c6c
Maliniara <84290119+Maliniara@users.noreply.github.com>
2024-07-12 00:14:37 +0200

    minor changes
    -added required to password entries
    -added password confirmation

--- Models/PasswordEntry.cs
+++ Models/PasswordEntry.cs
@@ -8,6 +8,14 @@ namespace PasswordManager.Models
         public int Id { get; set; }
         public string URL { get; set; }
         public string User { get; set; }
+        
+        [Required]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
     }
 }
